47e53db5b867641d8c4ab72073ba8b70ebb3076e
jayanthcr <c.r.jayanth7@gmail.com>
2022-11-08 02:10:46 +0530

    Completed upto Sprint 1

--- Project Development Phase/Sprint 1/dbactions/connection.py
+++ Project Development Phase/Sprint 1/dbactions/connection.py
@@ -3,7 +3,7 @@ import ibm_db
 def getConnection():
     conn = False
     try:
-        conn = ibm_db.connect('DATABASE=bludb;HOSTNAME=8e359033-a1c9-4643-82ef-8ac06f5107eb.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30120;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=jnq67868;PWD=JbJrbPr9U44SqIgx', '', '')
+        conn = ibm_db.connect('DATABASE=bludb;HOSTNAME=hostname;PORT=port;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=uid;PWD=pwd', '', '')
     except Exception as e:
         print("Exception while opening connection :",e)
         print(ibm_db.conn_errormsg())
